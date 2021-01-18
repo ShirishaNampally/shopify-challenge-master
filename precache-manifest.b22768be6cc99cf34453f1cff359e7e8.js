1b898fbdbccc2c3b97f8caaf3b768fbd102c38c2
Shirisha Nampally <shirishanampally96@gmail.com>
2021-01-18 06:45:53 +0530

    Updates

---
+++ precache-manifest.b22768be6cc99cf34453f1cff359e7e8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b5e2a5815fbd16213110d7bd7fc285b",
+    "url": "/shopify-challenge-master/index.html"
+  },
+  {
+    "revision": "0fb1b8e1b8301b6615cb",
+    "url": "/shopify-challenge-master/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "9cb67d938fb6d5962cf4",
+    "url": "/shopify-challenge-master/static/css/main.8a934eff.chunk.css"
+  },
+  {
+    "revision": "0fb1b8e1b8301b6615cb",
+    "url": "/shopify-challenge-master/static/js/2.5f6dddc2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shopify-challenge-master/static/js/2.5f6dddc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cb67d938fb6d5962cf4",
+    "url": "/shopify-challenge-master/static/js/main.009054cb.chunk.js"
+  },
+  {
+    "revision": "c96c0d021f7a8a201d33",
+    "url": "/shopify-challenge-master/static/js/runtime-main.9662c06e.js"
+  },
+  {
+    "revision": "c2e4f1c0fa44b32da1c3b5da688fbfb4",
+    "url": "/shopify-challenge-master/static/media/back.c2e4f1c0.svg"
+  },
+  {
+    "revision": "d79a8310ce4b557bf27d0d40ea846b89",
+    "url": "/shopify-challenge-master/static/media/cross.d79a8310.svg"
+  },
+  {
+    "revision": "6315845c9997b281babf13952f00f9d1",
+    "url": "/shopify-challenge-master/static/media/error.6315845c.svg"
+  },
+  {
+    "revision": "f448ccd718ec0069b15fc1e1b4661d0e",
+    "url": "/shopify-challenge-master/static/media/minus.f448ccd7.svg"
+  },
+  {
+    "revision": "1c7775bfca16bd458216de36ddeb1631",
+    "url": "/shopify-challenge-master/static/media/plus.1c7775bf.svg"
+  },
+  {
+    "revision": "35e7ec75ea4c3f92ba63576f851e2577",
+    "url": "/shopify-challenge-master/static/media/search.35e7ec75.svg"
+  }
+]);
